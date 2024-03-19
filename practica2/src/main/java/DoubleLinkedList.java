76d40e5d98b9bfe4100e5a6fde8214d2d6210395
Miki Galdeano Rodriguez <mikigaldeanorodriguez@MacBook-Pro-de-Miki.local>
2024-03-19 12:12:18 +0100

    Update DoubleLinkedList.java

--- practica2/src/main/java/DoubleLinkedList.java
+++ practica2/src/main/java/DoubleLinkedList.java
@@ -29,7 +29,8 @@ public class DoubleLinkedList<T> implements DoubleLinkedQueue<T> {
     @Override
     public void append(T value) {
         if(this.size == 0) {
-            this.first = new LinkedNode<T>(value, null, null);
+            this.last = new LinkedNode<T>(value, null, null);
+            this.first = this.last;
         } else {
             LinkedNode<T> newNode = new LinkedNode<T>(value, last, null);
             LinkedNode<T> oldLast = last;
@@ -43,38 +44,35 @@ public class DoubleLinkedList<T> implements DoubleLinkedQueue<T> {
     @Override
     public void deleteFirst() {
         if(this.size<1){
-            new DoubleLinkedQueueException("La lista está vacía");
+           throw new DoubleLinkedQueueException("La lista está vacía");
         }else if(this.size()==1){
             this.first = null;
             this.last = null;
         }else if(this.size()==2){
             this.first = this.last;
-            this.size = 1;
         }else{
             LinkedNode<T> aux = first.getNext();
             this.first = null;
             this.first = aux;
-            this.size--;
         }
+        this.size--;
     }
 
     @Override
     public void deleteLast() {
         if(this.size<1){
-            new DoubleLinkedQueueException("La lista está vacía");
+            throw new DoubleLinkedQueueException("La lista está vacía");
         }else if(this.size()==1){
             this.last = null;
             this.first = null;
-            this.size = 0;
         }else if(this.size()==2){
             this.last = this.first;
-            this.size = 1;
         }else{
             LinkedNode<T> aux = last.getPrevious();
             this.last = null;
             this.last = aux;
-            this.size--;
         }
+        this.size--;
     }
 
     @Override
